e7f5d24e84875f270d5a332a932123b3f4ae6d22
Nicholas Yip <nicholasyip@obceit.cc>
2021-05-30 17:41:12 +0900

    Guava 30.1.1 and Kotest 4.6.0

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -14,6 +14,6 @@ fun DependencyHandler.kotlinLibrary(module: String): Dependency =
 fun jacksonDataBinding() = "com.fasterxml.jackson.core:jackson-databind:$jackson_version"
 fun jacksonKotlinModule() = "com.fasterxml.jackson.module:jackson-module-kotlin:$jackson_version"
 
-fun guava() = "com.google.guava:guava:28.0-jre"
+fun guava() = "com.google.guava:guava:30.1.1-jre"
 
-fun kotlinTestJUnit5Runner() = "io.kotest:kotest-runner-junit5-jvm:4.4.3"
+fun kotlinTestJUnit5Runner() = "io.kotest:kotest-runner-junit5-jvm:4.6.0"
